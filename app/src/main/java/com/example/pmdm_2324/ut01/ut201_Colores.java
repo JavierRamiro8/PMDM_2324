cae013220b2ee13884610322f24be3ba527dddd1
javierRamiro8 <jramiroc04@gmail.com>
2023-09-28 23:25:53 +0200

    Ejercicio Colores

---
+++ app/src/main/java/com/example/pmdm_2324/ut01/ut201_Colores.java
@@ -0,0 +1,17 @@
+package com.example.pmdm_2324.ut01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pmdm_2324.R;
+
+public class ut201_Colores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ut201_colores);
+
+    }
+}
