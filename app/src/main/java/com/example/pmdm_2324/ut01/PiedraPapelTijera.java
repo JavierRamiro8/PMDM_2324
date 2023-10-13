b0280eed225c09e5313b623d70775598f3d7585f
javierramiro8 <jramiroc04@gmail.com>
2023-10-13 21:45:06 +0200

    PiedraPapelTijera Acabado

--- app/src/main/java/com/example/pmdm_2324/ut01/PiedraPapelTijera.java
+++ app/src/main/java/com/example/pmdm_2324/ut01/PiedraPapelTijera.java
@@ -5,57 +5,90 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.view.textclassifier.SelectionEvent;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.pmdm_2324.R;
 
-public class PiedraPapelTijera extends AppCompatActivity {
-Button piedra,papel,tijeras,reiniciar;
-TextView contadorJugador,contadorMaquina,resultadoMaquina,resultadoPartida;
+import java.util.Random;
 
-View.OnClickListener manejador;
+public class PiedraPapelTijera extends AppCompatActivity {
+    ImageButton piedra, papel, tijeras;
+    Button reiniciar;
+    TextView contadorJugador, contadorMaquina, resultadoMaquina, resultadoPartida;
+
+    View.OnClickListener manejador;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_piedra_papel_tijera);
-        piedra=findViewById(R.id.ut207btnnPiedra);
-        papel=findViewById(R.id.ut207btnnPapel);
-        tijeras=findViewById(R.id.ut207btnnTijeras);
-        reiniciar=findViewById(R.id.ut207btnnReiniciar);
-        contadorJugador=findViewById(R.id.ut207LbContadorJugador);
-        contadorMaquina=findViewById(R.id.ut207LbContadorMaquina);
-        resultadoMaquina=findViewById(R.id.ut207LbResultadoMaquina);
-        resultadoPartida=findViewById(R.id.ut207LbResultado);
+        piedra = findViewById(R.id.ut207btnnPiedra);
+        papel = findViewById(R.id.ut207btnnPapel);
+        tijeras = findViewById(R.id.ut207btnnTijeras);
+        reiniciar = findViewById(R.id.ut207btnnReiniciar);
+        contadorJugador = findViewById(R.id.ut207LbContadorJugador);
+        contadorMaquina = findViewById(R.id.ut207LbContadorMaquina);
+        resultadoMaquina = findViewById(R.id.ut207LbResultadoMaquina);
+        resultadoPartida = findViewById(R.id.ut207LbResultado);
 
-        manejador = ( View botonPulsado)->{
+        manejador = (View botonPulsado) -> {
             String tipoSeleccionado;
-            Button seleccionado= (Button) botonPulsado;
+            ImageButton seleccionado = (ImageButton) botonPulsado;
             seleccionado.setBackgroundColor(Color.BLACK);
-            int eleccionMaquina=(int) Math.random()*3;
-            int contadorJugador=0;
-            int contadorPC = 0;
-            while(eleccionMaquina==0){
-                eleccionMaquina=(int) Math.random()*3;
-            }
-            switch (eleccionMaquina){
-                case 1:
-                    tipoSeleccionado="Tijeras";
-                    resultadoMaquina.setText("La maquina a elegido Tijeras");
-                    if(tipoSeleccionado!=seleccionado.getText()){
-                        if(seleccionado.getText().equals("Papel")){
-                            resultadoPartida.setText("Has perdido");
-                            contadorPC++;
-                            contadorMaquina.setText(contadorPC);
-                        } else if (seleccionado.getText().equals("Piedra")){
-                            resultadoPartida.setText("Has Ganado");
-                            contadorJugador++;
-                            contadorMaquina.setText(contadorJugador);
-                        }
-                    }
+            String eleccionMaquina = obtenerEleccionMaquina();
+            String eleccionJugador = obtenerEleccionJugador(seleccionado);
+
+            resultadoMaquina.setText("La maquina a elegido " + eleccionMaquina);
+            if (eleccionMaquina.equals(eleccionJugador)) {
+                resultadoPartida.setText("Empate");
+            } else if (hasGanado(eleccionJugador, eleccionMaquina)) {
+                resultadoPartida.setText("Has Ganado");
+                actualizarContador(contadorJugador, 1);
+            } else {
+                resultadoPartida.setText("Has perdido");
+                actualizarContador(contadorMaquina, 1);
             }
         };
 
+        reiniciar.setOnClickListener((View v) -> {
+            contadorJugador.setText("0");
+            contadorMaquina.setText("0");
+            resultadoMaquina.setText("");
+            resultadoPartida.setText("");
+        });
+
+        piedra.setOnClickListener(manejador);
+        papel.setOnClickListener(manejador);
+        tijeras.setOnClickListener(manejador);
+    }
+
+    private String obtenerEleccionMaquina() {
+        String[] opciones = {"Piedra", "Papel", "Tijeras"};
+        int indice = new Random().nextInt(opciones.length);
+        return opciones[indice];
+    }
+
+    private String obtenerEleccionJugador(ImageButton seleccionado) {
+        if (seleccionado == piedra) {
+            return "Piedra";
+        } else if (seleccionado == papel) {
+            return "Papel";
+        } else if (seleccionado == tijeras) {
+            return "Tijeras";
+        }
+        return "";
+    }
+
+    private boolean hasGanado(String eleccionJugador, String eleccionMaquina) {
+        return (eleccionJugador.equals("Piedra") && eleccionMaquina.equals("Tijeras"))
+                || (eleccionJugador.equals("Papel") && eleccionMaquina.equals("Piedra"))
+                || (eleccionJugador.equals("Tijeras") && eleccionMaquina.equals("Papel"));
+    }
+
+    private void actualizarContador(TextView contador, int incremento) {
+        int valorActual = Integer.parseInt(contador.getText().toString());
+        contador.setText(String.valueOf(valorActual + incremento));
     }
 }
