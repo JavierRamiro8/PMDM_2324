8843e31e2ae5676899144fc783df5785a2341ded
JavierRamiro8 <jramiroc04@gmail.com>
2023-10-18 21:31:37 +0200

    Calculadora Acabada Definitivo

--- app/src/main/java/com/example/pmdm_2324/ut03/ut302CalculadoraAvanzada.java
+++ app/src/main/java/com/example/pmdm_2324/ut03/ut302CalculadoraAvanzada.java
@@ -6,10 +6,13 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
+
 import com.example.pmdm_2324.R;
 
 public class ut302CalculadoraAvanzada extends AppCompatActivity {
-    EditText primerNumero, SegundoNumero;
+    EditText primerNumero, segundoNumero;
+    TextView radioNoSeleccionado;
     RadioButton sumar, restar, multiplicar, dividir;
     Button botonCalcular;
 
@@ -17,8 +20,9 @@ public class ut302CalculadoraAvanzada extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ut302_calculadora_avanzada);
+        radioNoSeleccionado=findViewById(R.id.u302tvErrorRadioNoSeleccionado);
         primerNumero = findViewById(R.id.u302etPrimerNumero);
-        SegundoNumero = findViewById(R.id.u302etSegundoNumero);
+        segundoNumero = findViewById(R.id.u302etSegundoNumero);
         sumar = findViewById(R.id.u302rdSumar);
         restar = findViewById(R.id.u302rdRestar);
         multiplicar = findViewById(R.id.u302rdMultiplicar);
@@ -27,49 +31,27 @@ public class ut302CalculadoraAvanzada extends AppCompatActivity {
         botonCalcular.setOnClickListener(view -> {
             Intent i = new Intent(this, ut302CalculadoraResultado.class);
             double resultado = 0;
-            if (sumar.isChecked()) {
-                resultado = sumar();
-            } else if (restar.isChecked()) {
-                resultado = restar();
-            } else if (multiplicar.isChecked()) {
-                resultado = multiplicar();
-            } else if (dividir.isChecked()) {
-                resultado = dividir();
+            if (primerNumero.getText().toString().isEmpty()) {
+                primerNumero.setError("No se puede dejar vacio");
+            } else if (segundoNumero.getText().toString().isEmpty()) {
+                segundoNumero.setError("No se puede dejar vacio");
             }
-            i.putExtra("RESULTADO", resultado);
+            if( sumar.isChecked() || restar.isChecked() || multiplicar.isChecked() || dividir.isChecked()) {
+            if (sumar.isChecked()) {
+                i.putExtra("OPERACION", sumar.getText().toString());
+            } else if (restar.isChecked()) {
+                i.putExtra("OPERACION", restar.getText().toString());
+            } else if (multiplicar.isChecked()) {
+                i.putExtra("OPERACION", multiplicar.getText().toString());
+            } else if (dividir.isChecked()) {
+                i.putExtra("OPERACION", dividir.getText().toString());
+            }
+            i.putExtra("PRIMERNUMERO",primerNumero.getText().toString());
+            i.putExtra("SEGUNDONUMERO",segundoNumero.getText().toString());
             startActivity(i);
+        }else{
+              radioNoSeleccionado.setText("No se puede seguir sin una operacion seleccionada");
+            }
         });
-    }
-
-    private double sumar() {
-        double numero1 = Double.parseDouble(primerNumero.getText().toString());
-        double numero2 = Double.parseDouble(SegundoNumero.getText().toString());
-        double resultado = numero1 + numero2;
-        return resultado;
-    }
-
-    private double restar() {
-        double numero1 = Double.parseDouble(primerNumero.getText().toString());
-        double numero2 = Double.parseDouble(SegundoNumero.getText().toString());
-        double resultado = numero1 - numero2;
-        return resultado;
-    }
-
-    private double multiplicar() {
-        double numero1 = Double.parseDouble(primerNumero.getText().toString());
-        double numero2 = Double.parseDouble(SegundoNumero.getText().toString());
-        double resultado = numero1 * numero2;
-        return resultado;
-    }
-
-    private double dividir() {
-        final double divisionNula = 0.0;
-        double numero1 = Double.parseDouble(primerNumero.getText().toString());
-        double numero2 = Double.parseDouble(SegundoNumero.getText().toString());
-        if (numero2 == 0) {
-            return divisionNula;
-        }
-        double resultado = numero1 / numero2;
-        return resultado;
     }
 }
--- app/src/main/java/com/example/pmdm_2324/ut03/ut302CalculadoraResultado.java
+++ app/src/main/java/com/example/pmdm_2324/ut03/ut302CalculadoraResultado.java
@@ -1,24 +1,72 @@
 package com.example.pmdm_2324.ut03;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.TextView;
-
 import com.example.pmdm_2324.R;
 
-import org.w3c.dom.Text;
-
 public class ut302CalculadoraResultado extends AppCompatActivity {
-TextView resultadoCalculadora;
+    TextView resultadoCalculadora;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        resultadoCalculadora = findViewById(R.id.u302tvSalidaCalculadora);
         setContentView(R.layout.activity_ut302_calculadora_resultado);
-        resultadoCalculadora=findViewById(R.id.u302tvSalidaCalculadora);
-       double resultado=getIntent().getDoubleExtra("RESULTADO",0);
-       String cadena= String.valueOf(Double.valueOf(resultado));
-       resultadoCalculadora.setText(cadena);
+        resultadoCalculadora = findViewById(R.id.u302tvSalidaCalculadora);
+
+        String primerNumero = getIntent().getStringExtra("PRIMERNUMERO");
+        String segundoNumero = getIntent().getStringExtra("SEGUNDONUMERO");
+        double resultado = 0;
+        String operacion = getIntent().getStringExtra("OPERACION");
+        String cadena="";
+
+        if (operacion.equals("sumar")) {
+            resultado = sumar(primerNumero, segundoNumero);
+            cadena = String.valueOf(resultado);
+        } else if (operacion.equals("restar")) {
+            resultado = restar(primerNumero, segundoNumero);
+            cadena = String.valueOf(resultado);
+        } else if (operacion.equals("multiplicar")) {
+            resultado = multiplicar(primerNumero, segundoNumero);
+            cadena = String.valueOf(resultado);
+        } else if (operacion.equals("dividir")) {
+            if(primerNumero.equals("1") & segundoNumero.equals("0")){
+                cadena="Syntax Error";
+            }else{
+                resultado = dividir(primerNumero, segundoNumero);
+                cadena = String.valueOf(resultado);
+            }
+        }
+        resultadoCalculadora.setText(cadena);
+    }
+
+    public double sumar(String uno, String dos) {
+        double numero1 = Double.parseDouble(uno);
+        double numero2 = Double.parseDouble(dos);
+        return numero1 + numero2;
+    }
+
+    private double restar(String uno, String dos) {
+        double numero1 = Double.parseDouble(uno);
+        double numero2 = Double.parseDouble(dos);
+        return numero1 - numero2;
+    }
+
+    private double multiplicar(String uno, String dos) {
+        double numero1 = Double.parseDouble(uno);
+        double numero2 = Double.parseDouble(dos);
+        return numero1 * numero2;
+    }
+
+    private double dividir(String uno, String dos) {
+        final double divisionNula = 0.0;
+        double numero1 = Double.parseDouble(uno);
+        double numero2 = Double.parseDouble(dos);
+
+        if (numero1==1 && numero2 == 0) {
+            return divisionNula;
+        }
+        return numero1 / numero2;
     }
 }
+
