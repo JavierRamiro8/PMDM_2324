7baa844641158bcf9f80de9ed8ce99d6da99a5fe
JavierRamiro8 <jramiroc04@gmail.com>
2023-10-24 17:06:49 +0200

    Ejercicios Ejemplo Ida y vuelta de datos

---
+++ app/src/main/java/com/example/pmdm_2324/ejerciciosBasicos/EjemploBAsicoIdaYVueltaLanzada.java
@@ -0,0 +1,37 @@
+package com.example.pmdm_2324.ejerciciosBasicos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.pmdm_2324.R;
+
+public class EjemploBAsicoIdaYVueltaLanzada extends AppCompatActivity {
+    Button btEnviarNumero,btCancelar;
+    EditText etNumero;
+    public static final String CLAVE_NUMERO ="NOMBREKEYRANDOM" ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejemplo_basico_ida_yvuelta_lanzada);
+        btEnviarNumero=findViewById(R.id.btPracticaEnviarNumero);
+        btCancelar=findViewById(R.id.btPracticaCancelarNumero);
+        etNumero=findViewById(R.id.etPracticaIntroducirNumero);
+
+        btEnviarNumero.setOnClickListener(View->{
+            Intent data= new Intent();
+            data.putExtra(CLAVE_NUMERO,etNumero.getText().toString());
+            setResult(Activity.RESULT_OK,data);
+            finish();
+        });
+        btCancelar.setOnClickListener(View->{
+            setResult(Activity.RESULT_CANCELED);
+            finish();
+        });
+    }
+}
---
+++ app/src/main/java/com/example/pmdm_2324/ejerciciosBasicos/EjemploBasicoIdaYVueltaLanza.java
@@ -0,0 +1,58 @@
+package com.example.pmdm_2324.ejerciciosBasicos;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.pmdm_2324.R;
+
+import javax.xml.transform.Result;
+
+public class EjemploBasicoIdaYVueltaLanza extends AppCompatActivity {
+Button botonObtenerNumero;
+TextView tvNumero;
+TextView tvCancelado;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejemplo_basico_ida_yvuelta_lanza);
+        botonObtenerNumero=findViewById(R.id.btPracticaObtenerNumero);
+        tvNumero=findViewById(R.id.tvPracticaNumero);
+        tvCancelado=findViewById(R.id.tvPracticaSalidaCancelada);
+
+        botonObtenerNumero.setOnClickListener(View ->{
+            Intent i= new Intent(this,EjemploBAsicoIdaYVueltaLanzada.class);
+            launcher.launch(i);
+        });
+    }
+    ActivityResultLauncher<Intent> launcher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        tvNumero.setText(data.getStringExtra(EjemploBAsicoIdaYVueltaLanzada.CLAVE_NUMERO));
+                        tvCancelado.setText("");
+                    }else if(result.getResultCode() == Activity.RESULT_CANCELED){
+                        tvCancelado.setText("El usuario cancelo la operación");
+                        tvCancelado.setTextColor(Color.RED);
+                    }else{
+                        tvCancelado.setText("No tenemos codigo");
+                        tvCancelado.setTextColor(Color.RED);
+                    }
+                }
+            }
+    );
+}
