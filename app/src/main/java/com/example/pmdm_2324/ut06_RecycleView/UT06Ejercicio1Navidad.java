e806a05c6e0e9f6209cbb330937790ee8e3816d8
JavierRamiro8 <jramiroc04@gmail.com>
2024-01-10 23:05:45 +0100

    Remove app/build from Git tracking

---
+++ app/src/main/java/com/example/pmdm_2324/ut06_RecycleView/UT06Ejercicio1Navidad.java
@@ -0,0 +1,16 @@
+package com.example.pmdm_2324.ut06_RecycleView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pmdm_2324.R;
+
+public class UT06Ejercicio1Navidad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ut06_ejercicio1_navidad);
+    }
+}
