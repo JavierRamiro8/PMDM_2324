f51443e09074423db08341d987128e8deba72c50
JavierRamiro8 <jramiroc04@gmail.com>
2024-01-25 19:59:58 +0100

    MVVM prueba

---
+++ app/src/main/java/com/example/pmdm_2324/ut08/ut08A1NumeroAleatorioPrimo.java
@@ -0,0 +1,42 @@
+package com.example.pmdm_2324.ut08;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.pmdm_2324.R;
+
+public class ut08A1NumeroAleatorioPrimo extends AppCompatActivity {
+TextView tvNumero;
+Button btGenerar;
+ProgressBar pBCargando;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ut08_a1_numero_aleatorio_primo);
+       tvNumero=findViewById(R.id.ut08a1TvNumero);
+       btGenerar=findViewById(R.id.ut8a1BtLogin);
+       pBCargando=findViewById(R.id.ut08a1Carga);
+
+        ut08a1_numeroAleatorioViewModel vm=new ViewModelProvider(this).get(ut08a1_numeroAleatorioViewModel.class);
+        vm.getNumeros().observe(this,integer -> {
+            //Actualizas la interfaz en esta parte
+            tvNumero.setText(""+integer);
+            pBCargando.setVisibility(View.INVISIBLE);
+            tvNumero.setVisibility(View.VISIBLE);
+            btGenerar.setEnabled(true);
+        });
+        btGenerar.setOnClickListener(v -> {
+            pBCargando.setVisibility(View.VISIBLE);
+            tvNumero.setVisibility(View.INVISIBLE);
+            btGenerar.setEnabled(false);
+            vm.cargaNumero();
+        });
+    }
+}
---
+++ app/src/main/java/com/example/pmdm_2324/ut08/ut08a1_numeroAleatorioViewModel.java
@@ -0,0 +1,36 @@
+package com.example.pmdm_2324.ut08;
+
+import android.view.View;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.List;
+import java.util.Random;
+
+public class ut08a1_numeroAleatorioViewModel extends ViewModel {
+    private MutableLiveData<Integer> misDatos;
+    private final int DELAY=1000;
+    private final int MAX_NUM=10000;
+    public LiveData<Integer> getNumeros(){
+        if(misDatos==null){
+            misDatos=new MutableLiveData<Integer>();
+            cargaNumero();
+        }
+        return misDatos;
+    }
+
+    public void cargaNumero() {
+        Random random = new Random();
+        new Thread(()->{
+            try {
+                Thread.sleep((long)(Math.random()*DELAY)+DELAY);
+                int i=random.nextInt(MAX_NUM);
+                misDatos.postValue(i);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+    }
+}
