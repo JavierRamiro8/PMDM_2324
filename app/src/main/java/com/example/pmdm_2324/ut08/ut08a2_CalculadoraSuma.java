3220618e724f490c4701ef477813141c35c618ea
JavierRamiro8 <jramiroc04@gmail.com>
2024-01-28 12:49:30 +0100

    Calculadora MVVM

---
+++ app/src/main/java/com/example/pmdm_2324/ut08/ut08a2_CalculadoraSuma.java
@@ -0,0 +1,16 @@
+package com.example.pmdm_2324.ut08;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pmdm_2324.R;
+
+public class ut08a2_CalculadoraSuma extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ut08a2_calculadora_suma);
+    }
+}
